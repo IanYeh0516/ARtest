30ee5ebe287587400ce608539750a54e4c6e192d
iannn <a52j16@gmail.com>
2024-06-27 11:22:18 +0800

    add the click event

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const exampleModel = document.querySelector('#example-model');
+    const exampleText = document.querySelector('#example-text');
+
+    exampleModel.addEventListener("click", event => {
+        exampleText.setAttribute('value', 'success');
+    });
+
+  });
