9dad8d121db53cf72cddf036457e00e1ba2e2952
iannn <a52j16@gmail.com>
2024-06-28 15:10:23 +0800

    update animation model & method

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const exampleModel = document.querySelector('#example-model');
     const exampleText = document.querySelector('#example-text');
-
     exampleModel.addEventListener("click", event => {
         exampleText.setAttribute('value', 'success');
     });
-
   });
