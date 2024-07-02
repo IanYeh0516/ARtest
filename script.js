a96cf0683577f933469dce2783ad058b3d25b364
iannn <a52j16@gmail.com>
2024-07-02 16:05:23 +0800

    add new item&animation

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const exampleModel = document.querySelector('#example-model');
-    const exampleText = document.querySelector('#example-text');
+    const exampleModel = document.querySelector('#model-treasureBox');
     exampleModel.addEventListener("click", event => {
-        exampleText.setAttribute('value', 'success');
+        // 點選後設定animation-mixer屬性
+        exampleModel.setAttribute('animation-mixer', 'loop: once');
+        // 2秒後JV哥貓頭出現並觸發動畫
+        setTimeout(function() {
+            var testModel = document.getElementById('model-JVhead');
+            testModel.setAttribute('animation', 'property: scale; to: 1 1 1; dur: 2000; easing: easeInOutQuad');
+            exampleModel.setAttribute('animation', 'property: scale; to: 0 0 0; dur: 2000; easing: easeInOutQuad');
+
+        }, 2000);
     });
   });
